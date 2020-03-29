b822e75c60af790d1e5c45681a61d101e566ce21
evvvritt <evv.info@gmail.com>
2020-03-29 16:01:53 +0200

    viewer prt - basic scroll

--- src/components/Viewer--Prt.vue
+++ src/components/Viewer--Prt.vue
@@ -3,9 +3,11 @@
     //- .h-screen.sticky.bottom-0.left-0.border-b(v-for="n in 10", :style="{zIndex: 10 - n, background: n % 0 ? 'gray' : 'white'}")
       .h-screen.flex.items-center.px-6
         .w-full.pb-full(style="background:#999")
-    .h-screen.bg-white.shadow(v-for="(slice, i) in slices", :style="{zIndex: slices.length - i, height: '150vh'}", :class="{'sticky bottom-0 left-0 flex flex-col justify-end': i > 0, 'relative': !i}")
+    //- .h-screen.bg-white.shadow(v-for="(slice, i) in slices", :style="{zIndex: slices.length - i, height: '150vh'}", :class="{'sticky bottom-0 left-0 flex flex-col justify-end': i > 0, 'relative': !i}")
       .h-screen(:class="{'relative': i > 0, 'sticky top-0 left-0': !i}")
         component(:is="slice.slice_type", :data="slice")
+    .h-screen.relative(v-for="(slice, i) in slices")
+      component(:is="slice.slice_type", :data="slice")
 </template>
 
 <script>
