3c079be6deff172e9b20c6817a2059ee8dacd0b9
evvvritt <evv.info@gmail.com>
2020-04-16 13:55:26 +0200

    add vue-gtag

--- src/main.js
+++ src/main.js
@@ -6,11 +6,22 @@ import PrismicVue from 'prismic-vue'
 import linkResolver from './prismic-link-resolver'
 import './plugins/include-base-components'
 import Vue2TouchEvents from 'vue2-touch-events'
+import VueGtag from 'vue-gtag'
 
 Vue.use(PrismicVue, {
   endpoint: window.prismic.endpoint,
   linkResolver
 })
+
+// Google Analytics
+Vue.use(VueGtag, {
+  config: {
+    id: process.env.VUE_APP_GTAG_ID,
+    anonymize_ip: true
+  },
+  // whether to load script:
+  bootstrap: process.env.NODE_ENV === 'production' && window.navigator.doNotTrack !== '1'
+}, router)
 
 Vue.use(Vue2TouchEvents)
 
